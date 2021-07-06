cf8f4e7c8538bbecc3edd921211e3d9c81269d78
Shira Akler <shiraa@CISDOM.local>
2021-07-06 12:11:30 +0300

    Add project files.

---
+++ GiphyAPITest/TestGiphyAPI.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GiphyAPITest
+{
+    partial class TestGiphyAPI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnTrending = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(99, 114);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnTrending
+            // 
+            this.btnTrending.Location = new System.Drawing.Point(596, 114);
+            this.btnTrending.Name = "btnTrending";
+            this.btnTrending.Size = new System.Drawing.Size(75, 23);
+            this.btnTrending.TabIndex = 1;
+            this.btnTrending.Text = "Get Trend";
+            this.btnTrending.UseVisualStyleBackColor = true;
+            this.btnTrending.Click += new System.EventHandler(this.btnTrending_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(74, 72);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(53, 153);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(660, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnTrending);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnTrending;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ GiphyAPITest/TestGiphyAPI.cs
@@ -0,0 +1,68 @@
+﻿using FastMember;
+using GiphyAPIContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiphyAPITest
+{
+    public partial class TestGiphyAPI : Form
+    {
+        Dictionary<string, List<Gif>> SearchesGipfy = new Dictionary<string, List<Gif>>();
+        public TestGiphyAPI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrending_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IGiphyService> channel = new ChannelFactory<IGiphyService>("giphyEndpoint");
+            IGiphyService proxy = channel.CreateChannel();
+
+            List<Gif> gifs= proxy.GetAllGiphy();
+
+
+            channel.Close();
+            DataTable table = new DataTable();
+            using (var reader = ObjectReader.Create(gifs))
+            {
+                table.Load(reader);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (SearchesGipfy.ContainsKey(txtSearch.Text))
+            {
+                
+                DataTable table = new DataTable();
+                using (var reader = ObjectReader.Create(SearchesGipfy[txtSearch.Text]))
+                {
+                    table.Load(reader);
+                }
+            }
+            else
+            {
+                ChannelFactory<IGiphyService> channel = new ChannelFactory<IGiphyService>("giphyEndpoint");
+                IGiphyService proxy = channel.CreateChannel();
+
+                List<Gif> gifs = proxy.SearchGigphy(txtSearch.Text);
+
+                channel.Close();
+                SearchesGipfy.Add(txtSearch.Text, gifs);
+                DataTable table = new DataTable();
+                using (var reader = ObjectReader.Create(gifs))
+                {
+                    table.Load(reader);
+                }
+            }
+        }
+    }
+}
